3aed5a79c435cad6b44e4c2277727c66cfe3f50e
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2021-07-15 17:16:20 +0200

    Editing covid_19_death_risk.py

--- Assignment/covid_19_death_risk.py
+++ Assignment/covid_19_death_risk.py
@@ -1,17 +1,13 @@
-
-###### User dan bilgi alarak olusturulan risk analizi #########
-
 age = (input('Are you a cigarette addict older than 75 years old?: ').title())
 chronic = (input('Do you have a severe chronic disease?: ').title())
-
 immune = (input('Is your immune system too weak?:').title())
 
 if age == 'Yes' or chronic == 'Yes' or immune == 'Yes' :
 
-     print(True , 'You are in Risk Group')
+     print('You are in Risk Group')
 
 else :
-    print(False , 'You are not in Risk Group')
+    print('You are not in Risk Group')
 
 
    
