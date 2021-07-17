b32a6d559e2bd5197154f159dfdb0a2b51c9ebda
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2021-07-18 00:35:35 +0200

    Finding Armstronggit status  number

---
+++ Armstrong_number.py
@@ -0,0 +1,19 @@
+num = (input('pls enter a number\t:'))
+
+digit = str(num)
+summ = 0
+if num.isdigit():
+    for i in digit :
+        num_1 = int(i) ** len(digit)
+        summ +=num_1
+        num = int(num)
+    if num == summ :
+            print(f'{num} is Amrstrong number !!!!')
+    else:
+            print(f'{num} is not Armstrong number')
+elif num.isalpha():
+    print(' It is an invalid entry. Dont use non-numeric, float, or negative values!')
+elif float(num) < 0:
+     print(' It is an invalid entry. Dont use non-numeric, float, or negative values!')
+else:
+    print('It is an invalid entry. Dont use non-numeric, float, or negative values!')
