5d36de3d00225e0a02a41e5efa27ad2c6e667a92
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2021-07-17 11:23:50 +0200

    Anagram(words with same letter)

---
+++ anagrams.py
@@ -0,0 +1,12 @@
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+dict ={}
+
+for i in words:
+    sortedWord = ''.join(sorted(i)) 
+    if sortedWord in  dict:
+        dict[sortedWord].append(i)
+    else :
+        dict[sortedWord] = [i]
+
+print(list(dict.values()))
