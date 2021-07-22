d34b0bfd55dc530121828de943d6989da6734669
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2021-07-23 00:29:02 +0200

    finding prime number updated

--- Assignment/finding_prime_number.py
+++ Assignment/finding_prime_number.py
@@ -1,12 +1,15 @@
 prime = int(input('pls enter a number\t:'))
 sign = False
 
-for i in range(2 , int(prime/2)+1):
-    if prime % i == 0 :
-        sign = True
-        break
-if sign:
-    print('{} is not prime number'.format(prime)) 
-else:
-    print('{} is prime number'.format(prime))  
+if prime > 0:
+    for i in range(2 , int(prime/2)+1):
+        if prime % i == 0 :
+            sign = True
+            break
+    if sign:
+        print('{} is not prime number'.format(prime)) 
+    else:
+        print('{} is prime number'.format(prime))  
        
+else :
+    print('Please enter a number greater than 0')
